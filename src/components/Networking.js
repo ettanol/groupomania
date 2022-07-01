351daa4d5dbe3086a961d663880b256066471f80
ettanol <christopher.lanotte@outlook.com>
2022-07-01 10:48:12 +0200

    like function added

--- src/components/Networking.js
+++ src/components/Networking.js
@@ -1,5 +1,54 @@
 import { useState } from "react"
-import { FaWindowClose } from 'react-icons/fa'
+import { FaWindowClose,
+            FaThumbsUp,
+            FaThumbsDown } from 'react-icons/fa'
+
+const Modal = ({ post, setShowModal, onEditPost }) => {
+    const [modifiedPost, setModifiedPost] = useState(post)
+        return(
+        <div className="modal-screen">
+            <FaWindowClose className = "modal-close" onClick={() => setShowModal(false)}/>
+            <div className="modal-modify">
+                <textarea className="publication-value" onChange={(e)=> setModifiedPost({...modifiedPost, value: e.target.value})}>{modifiedPost.value}</textarea>
+                <button type="button" className="modify-button"
+                onClick={() => {
+                    setShowModal(false)
+                    onEditPost(modifiedPost)
+                }
+                }>Modifier</button>
+            </div>
+        </div>)
+}
+
+const Thumbs = (props) => {
+    const [likeClicked, setLikeClicked] = useState(false)
+    const [dislikeClicked, setDislikeClicked] = useState(false)
+    const [like, setLike] = useState(props.like)
+    const [dislike, setDislike] = useState(props.dislike)
+
+    return (
+        <div className="thumbs">
+            <FaThumbsUp className="thumbs_up" onClick={() => {
+                likeClicked === false ? setLikeClicked(true) : setLikeClicked(false)
+                if(dislikeClicked){
+                    setDislikeClicked(false)
+                    setDislike(dislike + 1)
+                }  
+                likeClicked ? setLike(like - 1) : setLike(like + 1)
+            }} style={{color : likeClicked ? 'green' : '#777'}}/>
+            <span>{like}</span>
+            <FaThumbsDown className="thumbs_down" onClick={() => {
+                dislikeClicked === false ? setDislikeClicked(true) : setDislikeClicked(false)
+                if(likeClicked){
+                    setLikeClicked(false)
+                    setLike(like - 1)
+                }  
+                dislikeClicked ? setDislike(dislike + 1) : setDislike(dislike - 1)
+            }} style={{color : dislikeClicked ? 'red' : '#777'}}/>
+            <span>{dislike}</span>
+        </div>
+    )
+}
 
 const Networking = () => {
     const [input, setInput] = useState("")
@@ -12,11 +61,21 @@ const Networking = () => {
                 _id : 1,
                 value: "Bienvenue sur le forum de partage de votre entreprise",
                 user : "John Doe",
+                likes: 2,
+                dislikes: 0,
+                usersLiked: ["John Doe", "Colonel Moutarde"],
+                usersDisliked: [],
+                timeOfUpload: 1,
             },
             {
                 _id : 2,
                 value: "Wow, super cool de pouvoir discuter avec vous",
                 user : "Colonel Moutarde",
+                likes: 2,
+                dislikes: 0,
+                usersLiked: ["John Doe", "Colonel Moutarde"],
+                usersDisliked: [],
+                timeOfUpload: 2,
             },
         ]
     )
@@ -31,27 +90,23 @@ const Networking = () => {
                 _id : posts.length + 1,
                 value: input,
                 user: "John Doe",
+                likes: 0,
+                dislikes: 0,
+                usersLiked: [],
+                usersDisliked: [],
+                timeOfUpload: new Date(),
             }
             setPosts([...posts, post])
         }
     }
-    const Modal = () => {
-            if(showModal)  {
-                return(
-                <div className="modal-screen">
-                    <FaWindowClose className = "modal-close" onClick={() => setShowModal(false)}/>
-                    <div className="modal-modify">
-                        <textarea className="publication-value">{post.value}</textarea>
-                        <button type="button" className="modify-button"
-                        onClick={() => {
-                            setShowModal(false)
-                            setPost("")
-                        }
-                        }>Modifier</button>
-                    </div>
-                </div>)
-            }
+
+    const onEditPost = (post) => {
+        const index = posts.findIndex(p => p._id === post._id)
+        if (index !== -1){
+        posts[index] = post
+        setPosts(posts)
         }
+    }
   return (
     <div className="networking">
         <form className="publish-post"
@@ -61,27 +116,32 @@ const Networking = () => {
                 setInput("")
             }}>
             <textarea type="text" className="publish-text__area" name="post" placeholder="Une idée à partager ?" value={input} maxLength="250" onChange={handleChange}/>
+            <label htmlFor="image_button" className="image_button">Ajouter une image</label>
+            <input type="file" id="image_button" accept=".png, .jpg, .jpeg, .gif" />
             <button type="submit" 
                 className="submitPost">Envoyer</button>
         </form>
-        <Modal />
-        <div className="publications">{
-            posts.map( post => {
-            return (
-                <div className="publication" key={post._id}>
-                    <div className="publication-value">{post.value}</div>
-                    <div className="user">{post.user}</div>
-                    <button type="button" className="modify-button"
-                    onClick={ () => setShowModal(true) }
-                    >Modifier</button>
-                    <button type="button" className="delete-button"
-                    onClick={(e) => {
-                        e.target.closest('.publication').remove()
-                    }}>Supprimer</button>
-                    </div>
-            )}
-            )
-        }
+        {showModal && <Modal post={post} setShowModal={setShowModal} onEditPost={onEditPost}/>} 
+        <div className="publications">
+            {
+                posts.map( post => {
+                    return (
+                        <div className="publication" key={post._id}>
+                        <div className="publication-value">{post.value}</div>
+                        <div className="user">{post.user}</div>
+                        <Thumbs like={post.likes} dislike={post.dislikes}/>
+                        <button type="button" className="modify-button"
+                        onClick={ () => {setShowModal(true)
+                                            setPost(post)} }
+                        >Modifier</button>
+                        <button type="button" className="delete-button"
+                        onClick={(e) => {
+                            e.target.closest('.publication').remove()
+                        }}>Supprimer</button>
+                        </div>
+                )}
+                ).sort(post => -(post.timeOfUpload))
+            }
         </div>
     </div>
   )
